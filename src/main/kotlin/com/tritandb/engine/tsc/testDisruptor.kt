53e994a4830ab370cc7b2a012833c0be899e2e1e
Eugene Siow <kyo116@gmail.com>
2017-05-17 18:06:16 +0100

    Add zmq

--- src/main/kotlin/com/tritandb/engine/tsc/testDisruptor.kt
+++ src/main/kotlin/com/tritandb/engine/tsc/testDisruptor.kt
@@ -1,19 +1,13 @@
 package com.tritandb.engine.tsc
 
-import com.lmax.disruptor.EventFactory
-import com.lmax.disruptor.EventHandler
-import com.lmax.disruptor.RingBuffer
-import com.lmax.disruptor.dsl.Disruptor
-import com.tritandb.engine.tsc.data.*
 import com.tritandb.engine.tsc.data.EventProtos.TritanEvent.EventType.CLOSE
 import com.tritandb.engine.tsc.data.EventProtos.TritanEvent.EventType.INSERT
-import com.tritandb.engine.util.BitByteBufferWriter
-import com.tritandb.engine.util.BitOutput
+import com.tritandb.engine.tsc.data.buildRow
+import com.tritandb.engine.tsc.data.buildRows
+import com.tritandb.engine.tsc.data.buildTritanEvent
+import org.zeromq.ZMQ
 import java.io.BufferedReader
-import java.io.File
 import java.io.FileReader
-import java.io.OutputStream
-import java.util.concurrent.Executors
 import kotlin.system.measureTimeMillis
 
 
@@ -22,78 +16,50 @@ import kotlin.system.measureTimeMillis
 * Created by eugene on 12/05/2017.
 */
 
-val o: OutputStream = File("shelburne.tsc").outputStream()
-//val b: BitOutput = BitWriter(o)
-val b: BitOutput = BitByteBufferWriter(o)
-val c: CompressorFlat = CompressorFlat(1271692742104,b,6)
-//var count = 0
-
-val handler:EventHandler<DisruptorEvent> = EventHandler({ (tEvent), _, _ ->
-    when(tEvent.type) {
-        INSERT -> {
-            for (row in tEvent.rows.rowList) {
-                c.addRow(row.timestamp, row.valueList)
-            }
-        }
-        CLOSE -> {
-            c.close()
-            println("closed")
-        }
-        else -> {
-        }
-    }
-})
-
 fun main(args: Array<String>) {
-    // Executor that will be used to construct new threads for consumers
-    val executor = Executors.newSingleThreadExecutor()
+    val context = ZMQ.context(2)
+    val sender = context.socket(ZMQ.PUSH)
+    sender.connect("tcp://localhost:5700")
 
-    // Specify the size of the ring buffer, must be power of 2.
-    val bufferSize = 1024
+    Thread.sleep(1000)
 
-    // Construct the Disruptor
-    @Suppress("DEPRECATION")
-    val disruptor = Disruptor(EventFactory ({ DisruptorEvent() }), bufferSize, executor) //TODO: change to threadfactory
+    println("Time: ${measureTimeMillis{shelburne(sender)}}")
 
-    // Connect the handler
-    disruptor.handleEventsWith(handler)
-
-    // Start the Disruptor, starts all threads running
-    disruptor.start()
-
-    // Get the ring buffer from the Disruptor to be used for publishing.
-    val ringBuffer = disruptor.ringBuffer
-
-    println("Time: ${measureTimeMillis{shelburne(ringBuffer)}}")
+    sender.close()
+    context.close()
 }
 
-fun shelburne(ringBuffer: RingBuffer<DisruptorEvent>) {
-    val br = BufferedReader(FileReader("/Users/eugene/Documents/Programming/data/shelburne/shelburne_test.csv"))
+fun shelburne(sender:ZMQ.Socket) {
+
+
+    val br = BufferedReader(FileReader("/Users/eugene/Documents/Programming/data/shelburne/shelburne.csv"))
     br.readLine() //header
     for(line in br.lines()) {
+        var addThis = true
         val parts = line.split(",")
         if(parts.size>6) {
-            ringBuffer.publishEvent { event, _ ->
-                event.value = buildTritanEvent {
-                    type = INSERT
-                    name = "shelburne"
-                    rows = buildRows {
-                        addRow(buildRow {
-                            timestamp = (parts[0].toLong()/1000000)
-                            for(i in 1..6) {
-                                if(parts[i] == "")
-                                    return@publishEvent
+            val event = buildTritanEvent {
+                type = INSERT
+                name = "shelburne"
+                rows = buildRows {
+                    addRow(buildRow {
+                        timestamp = (parts[0].toLong() / 1000000)
+                        for(i in 1..6) {
+                            if (parts[i] == "")
+                                addThis = false
+                            else
                                 addValue(java.lang.Double.doubleToLongBits(parts[i].toDouble()))
-                            }
-                        })
-                    }
+                        }
+                    })
                 }
             }
+            if(addThis)
+                sender.send(event.toByteArray())
         }
     }
 
-    ringBuffer.publishEvent { event, _ -> event.value = buildTritanEvent {
+    sender.send(buildTritanEvent {
         type = CLOSE
         name = "shelburne"
-    } }
+    }.toByteArray())
 }
