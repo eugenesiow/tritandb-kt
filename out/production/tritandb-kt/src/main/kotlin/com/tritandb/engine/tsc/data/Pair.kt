fcf252e2dc91f311032ff9fe6d9e28a6331746d3
Eugene Siow <kyo116@gmail.com>
2017-07-05 11:03:00 +0100

    Refactor EventProtos to main java

--- out/production/tritandb-kt/src/main/kotlin/com/tritandb/engine/tsc/data/Pair.kt
+++
@@ -1,8 +0,0 @@
-package main.kotlin.com.tritandb.engine.tsc.data
-
-/**
- * Created by eugene on 11/05/2017.
- */
-data class Pair(val timestamp:Long, val value:Long) {
-    fun getDoubleValue():Double = java.lang.Double.longBitsToDouble(value)
-}
--- out/production/tritandb-kt/src/main/kotlin/com/tritandb/engine/tsc/data/Row.kt
+++
@@ -1,8 +0,0 @@
-package main.kotlin.com.tritandb.engine.tsc.data
-
-/**
- * Created by eugene on 11/05/2017.
- */
-data class Row(val timestamp:Long, val values:LongArray) {
-    fun getRow():List<Pair> = values.map { value -> Pair(timestamp,value) }
-}
